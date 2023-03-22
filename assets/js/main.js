f04e3ebf134e9c055f4879089d884e295b18ef90
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-22 17:27:41 +0100

    [add] ciclo for che aggiunge classi

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,18 @@ const container = document.querySelector('.container');
 for(let i = 1; i <= 100; i++){
   const box = document.createElement('div');
   box.classList.add('box');
+  box.innerHTML = i;
   container.appendChild(box);
+
+  if(!(i % 3) && !(i % 5)){
+    box.classList.add('fizz')
+    box.classList.add('buzz')
+    box.innerHTML = 'FIZZBUZZ!';    
+  }else if (!(i % 5)){
+    box.classList.add('buzz')
+    box.innerHTML = 'BUZZ';  
+  }else if (!(i % 3)){
+    box.classList.add('fizz')
+    box.innerHTML = 'FIZZ';  
+  }
 }
